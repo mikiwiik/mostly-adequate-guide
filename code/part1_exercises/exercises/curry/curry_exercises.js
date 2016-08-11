38f39fd0149c310cb6c25753984bf7b84d87b16e
Miki Wiik <miki.wiik@gmail.com>
2016-08-11 13:24:12 +0300

    Complete Exercise 2

--- code/part1_exercises/exercises/curry/curry_exercises.js
+++ code/part1_exercises/exercises/curry/curry_exercises.js
@@ -19,12 +19,9 @@ var sentences = map(words);
 //==============
 // Refactor to remove all arguments by partially applying the functions
 
-var filterQs = function (xs) {
-    return filter(function (x) {
-        return match(/q/i, x);
-    }, xs);
-};
-
+var filterQs = filter(function (x) {
+    return match(/q/i, x);
+});
 
 // Exercise 3
 //==============
