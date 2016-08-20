84d4377bcf8b92fa08063c4418a3dfb99e3ff269
Miki Wiik <miki.wiik@gmail.com>
2016-08-20 13:20:15 +0300

    Functor exercises: complete exercise 8

--- code/part2_exercises/exercises/functors/functor_exercises.js
+++ code/part2_exercises/exercises/functors/functor_exercises.js
@@ -107,6 +107,12 @@ var save = function (x) {
     });
 };
 
-var ex8 = undefined;
+var ex8 = _.compose(
+    either(
+        IO.of, // Simply wrap the value of left in a IO
+        save
+    ),
+    ex7
+);
 
 module.exports = {ex1: ex1, ex2: ex2, ex3: ex3, ex4: ex4, ex5: ex5, ex6: ex6, ex7: ex7, ex8: ex8};
